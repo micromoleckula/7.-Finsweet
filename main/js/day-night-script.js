94aaedb8fb1f469c965ab7fd7f49ab4969ff3832
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-02-04 22:23:59 +0200

    fix night theme

--- main/js/day-night-script.js
+++ main/js/day-night-script.js
@@ -2,11 +2,11 @@ let dayNight = document.getElementById('style-css');
 let btnTheme = document.getElementById('btn-dark');
 
 btnTheme.onclick = function () {
-  if (dayNight.getAttribute("href") == "/main/styles/day/style.css") {
-    dayNight.href = "/main/styles/night/style.css";
+  if (dayNight.getAttribute("href") == "/main/styles/day.css") {
+    dayNight.href = "/main/styles/night.css";
     console.log('night')
   } else {
-    dayNight.href = "/main/styles/day/style.css";
+    dayNight.href = "/main/styles/day.css";
     console.log('day')
   }
   console.log('day-night')
--- main/js/scroll.js
+++ main/js/scroll.js
@@ -3,18 +3,27 @@
   const header_colorLogo = document.querySelector('.logotype');
   const header_color = document.querySelectorAll('.header-a');
   let dayNight = document.getElementById('style-css');
+  let burger1 = document.querySelector('.burger-item-line1');
+  let burger2 = document.querySelector('.burger-item-line2');
+  let burger3 = document.querySelector('.burger-item-line3');
 
   window.onscroll = () => {
     if (window.scrollY > 50) {
       header_active.classList.add('header_active');
       header_colorLogo.style.color = 'white';
-      
+      burger1.style.background = 'white';
+      burger2.style.background = 'white';
+      burger3.style.background = 'white';
+
       header_color.forEach(element => {
         element.style.color = 'white';
-      });
-    } else if (dayNight.getAttribute("href") == "/main/styles/day/style.css") {
+      })
+    } else if (dayNight.getAttribute("href") == "/main/styles/day.css") {
       header_active.classList.remove('header_active');
       header_colorLogo.style.color = 'black';
+      burger1.style.background = 'black';
+      burger2.style.background = 'black';
+      burger3.style.background = 'black';
       
       header_color.forEach(element => {
         element.style.color = 'black';
@@ -22,6 +31,9 @@
     } else {
       header_active.classList.remove('header_active');
       header_colorLogo.style.color = 'white';
+      burger1.style.background = 'white';
+      burger2.style.background = 'white';
+      burger3.style.background = 'white';
       
       header_color.forEach(element => {
         element.style.color = 'white';
