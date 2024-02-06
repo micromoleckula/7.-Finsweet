432d697d6bc2f366b94f286a8744779893750d51
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2024-02-06 16:48:49 +0200

    fix bags

--- main/js/scroll.js
+++ main/js/scroll.js
@@ -3,19 +3,15 @@
   const header_colorLogo = document.querySelector('.logotype');
   const header_color = document.querySelectorAll('.header-a');
   let dayNight = document.getElementById('style-css');
-  let burger = document.querySelector('.burger-item-line1');
 
   window.onscroll = () => {
     if (window.scrollY > 50) {
       header_active.classList.add('header_active');
       header_colorLogo.style.color = 'white';
-      burger1.style.background = 'white';
-      burger2.style.background = 'white';
-      burger3.style.background = 'white';
 
-      burger.forEach(index => {
-        index.style.background = 'white';
-      })
+      document.querySelectorAll('.burger-item-close div, .burger-item div').forEach(item => {
+        item.style.background = 'white';
+    });
         
       
       header_color.forEach(element => {
@@ -26,9 +22,9 @@
       header_active.classList.remove('header_active');
       header_colorLogo.style.color = 'black';
       
-      burger.forEach(index => {
-        index.style.background = 'black';
-      })
+      document.querySelectorAll('.burger-item-close div, .burger-item div').forEach(item => {
+        item.style.background = 'black';
+    });
       
       header_color.forEach(element => {
         element.style.color = 'black';
@@ -38,9 +34,9 @@
       header_active.classList.remove('header_active');
       header_colorLogo.style.color = 'white';
       
-      burger.forEach(index => {
-        index.style.background = 'white';
-      })
+      document.querySelectorAll('.burger-item-close div, .burger-item div').forEach(item => {
+        item.style.background = 'white';
+    });
       
       header_color.forEach(element => {
         element.style.color = 'white';
